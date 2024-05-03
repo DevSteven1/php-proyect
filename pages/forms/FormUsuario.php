3739fdc5c91aa044f5bb81559776ef5b40094732
estefanyt20 <tovarestefani59@gmail.com>
2024-05-03 11:35:31 -0500

    form Usuario Actualizacion

--- pages/forms/FormUsuario.php
+++ pages/forms/FormUsuario.php
@@ -1,58 +1,71 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Usuario</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-<div class="container">
-  <div class="row">
-    <div class="col-md-6">
-  <div class="mb-3 mt-3">
-  <label for="exampleFormControlInput1" class="form-label">ID</label>
-  <input type="number" name="id" class="form-control"  placeholder="Ingrese Su Cedula">
-</div>
 
-<div class="mb-3 mt-3">
-  <label for="exampleFormControlInput1" class="form-label">Nombre</label>
-  <input type="text" name="nombre" class="form-control"  placeholder="Ingrese Su Nombre">
-</div>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Usuario</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
 
-<div class="mb-3 mt-3">
-  <label for="exampleFormControlInput1" class="form-label">Apellidos</label>
-  <input type="text" name="apellido" class="form-control"  placeholder="Ingrese Su Apellido">
-</div>
+<body>
 
+
+  <div class="container mt-3" style="font-family: serif">
+
+    <div class="card">
+      <h5 class="card-header">Formulario Usuario</h5>
+      <div class="card-body">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="mb-3 mt-3">
+                <label for="exampleFormControlInput1" class="form-label">ID</label>
+                <input type="number" name="id" class="form-control" placeholder="Ingrese Su Cedula">
+              </div>
+
+              <div class="mb-3 mt-3">
+                <label for="exampleFormControlInput1" class="form-label">Nombre</label>
+                <input type="text" name="nombre" class="form-control" placeholder="Ingrese Su Nombre">
+              </div>
+
+              <div class="mb-3 mt-3">
+                <label for="exampleFormControlInput1" class="form-label">Apellidos</label>
+                <input type="text" name="apellido" class="form-control" placeholder="Ingrese Su Apellido">
+              </div>
+
+            </div>
+            <div class="col-md-6">
+              <div class="mb-3 mt-3">
+                <label for="exampleFormControlInput1" class="form-label">Telefono</label>
+                <input type="text" name="telefono" class="form-control" placeholder="Ingrese Su Telefono">
+              </div>
+
+              <div class="mb-3 mt-3">
+                <label for="exampleFormControlInput1" class="form-label">correo Electronico</label>
+                <input type="text" name="correo" class="form-control" placeholder="Ingrese Su Correo">
+              </div>
+
+              <div class="mb-3 mt-3">
+                <label for="exampleFormControlInput1" class="form-label">Clave</label>
+                <input type="password" name="clave" class="form-control" placeholder="Ingrese Su Clave">
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+
+        <a href="#" class="btn btn-primary">Registrar</a>
+      </div>
     </div>
-    <div class="col-md-6">
-<div class="mb-3 mt-3">
-  <label for="exampleFormControlInput1" class="form-label">Telefono</label>
-  <input type="text" name="telefono" class="form-control"  placeholder="Ingrese Su Telefono">
-</div>
 
-<div class="mb-3 mt-3">
-  <label for="exampleFormControlInput1" class="form-label">correo Electronico</label>
-  <input type="text" name="correo" class="form-control"  placeholder="Ingrese Su Correo">
-</div>
-
-<div class="mb-3 mt-3">
-  <label for="exampleFormControlInput1" class="form-label">Clave</label>
-  <input type="password" name="clave" class="form-control"  placeholder="Ingrese Su Clave">
-</div>
-    </div>
-</div>
-
-</div>
-
-
-    
+  </div>
+  </div>
 
 
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
 </html>
